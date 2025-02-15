c9e560049aa700f3be7002508f5b0c0212f7ad7c
Hamoody <hammadalansarii@gmail.com>
2025-02-15 16:47:19 +0530

    add comments in clue.py

--- clue.py
+++ clue.py
@@ -1,17 +1,22 @@
+# Need to determine exactly one suspect, one room, and one weapon
+
 import termcolor
 
 from logic import *
 
+# Suspects
 mustard = Symbol("ColMustard")
 plum = Symbol("ProfPlum")
 scarlet = Symbol("MsScarlet")
 characters = [mustard, plum, scarlet]
 
+# Rooms
 ballroom = Symbol("ballroom")
 kitchen = Symbol("kitchen")
 library = Symbol("library")
 rooms = [ballroom, kitchen, library]
 
+# Weapons
 knife = Symbol("knife")
 revolver = Symbol("revolver")
 wrench = Symbol("wrench")
@@ -30,7 +35,7 @@ knowledge = And(
     Or(mustard, plum, scarlet),
     Or(ballroom, kitchen, library),
     Or(knife, revolver, wrench),
-)
+) # At least one of each category is true
 
 knowledge.add(Not(mustard))
 knowledge.add(Not(kitchen))
@@ -38,9 +43,9 @@ knowledge.add(Not(revolver))
 
 knowledge.add(Or(
     Not(scarlet), Not(library), Not(wrench)
-))
+)) # At least one of these is true
 
-knowledge.add(Not(plum))
+knowledge.add(Not(plum)) 
 knowledge.add(Not(ballroom))
 
 check_knowledge(knowledge)
