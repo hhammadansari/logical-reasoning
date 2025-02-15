96f55b48cd83d28e0aaf2d0892cb6e342408bbac
Hamoody <hammadalansarii@gmail.com>
2025-02-15 18:09:26 +0530

    minor change

--- harry.py
+++ harry.py
@@ -1,5 +1,3 @@
-# This program uses the logic module to determine if it is raining outside based on the following rules:
-
 # If it is not raining, then Hagrid is outside.
 # If it is not raining, then Dumbledore may be outside.
 # Hagrid and Dumbledore cannot be outside together.
